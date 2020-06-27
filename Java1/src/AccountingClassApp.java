243f9cc6ea76732ad6930e8053b720d966c3c162
YuuJungg <65105002+YuuJungg@users.noreply.github.com>
2020-06-27 14:19:45 +0900

    Add files via upload

--- Java1/src/AccountingClassApp.java
+++ Java1/src/AccountingClassApp.java
@@ -1,60 +1,60 @@
 class Accounting{
-	public static double valueOfSupply;
-	public static double vatRate;
-	public static double expenseRate;
-	
-	static void print() {
-		System.out.println("Value of supply : " + valueOfSupply); // 공급가		
-		System.out.println("VAT : " + getVAT()); // 부가가치세
-		System.out.println("Total : " + getTotal()); // 공급가 + 부가가치세
-		System.out.println("Expense : " + getExpense()); // 비용
-		System.out.println("Income : " + getIncome()); // 공급가
-		System.out.println("Dividend 1 : " + getDiviend1());
-		System.out.println("Dividend 2 : " + getDiviend2());
-		System.out.println("Dividend 3 : " + getDiviend3());
-	}
-
-	private static double getDiviend1() {
-		return getIncome() * 0.5;
-	}
-	
-	private static double getDiviend2() {
-		return getIncome() * 0.3;
-	}
-	
-	private static double getDiviend3() {
-		return getIncome() * 0.2;
-	}
-	
-	private static double getIncome() {
-		return valueOfSupply - getExpense();
-	}
-
-	private static double getExpense() {
-		return valueOfSupply*expenseRate;
-	}
-
-	private static double getTotal() {
-		return valueOfSupply + getVAT();
-	}
-
-	private static double getVAT() {
-		return valueOfSupply*vatRate;
-	}
-	
+    public double valueOfSupply;
+    public double vatRate;
+    public double expenseRate;
+    public void print() {
+        System.out.println("Value of supply : " + valueOfSupply);
+        System.out.println("VAT : " + getVAT());
+        System.out.println("Total : " + getTotal());
+        System.out.println("Expense : " + getExpense());
+        System.out.println("Income : " + getIncome());
+        System.out.println("Dividend 1 : " + getDiviend1());
+        System.out.println("Dividend 2 : " + getDiviend2());
+        System.out.println("Dividend 3 : " + getDiviend3());
+    }
+ 
+    public double getDiviend1() {
+        return getIncome() * 0.5;
+    }
+    public double getDiviend2() {
+        return getIncome() * 0.3;
+    }
+    public double getDiviend3() {
+        return getIncome() * 0.2;
+    }
+ 
+    public double getIncome() {
+        return valueOfSupply - getExpense();
+    }
+ 
+    public double getExpense() {
+        return valueOfSupply * expenseRate;
+    }
+ 
+    public double getTotal() {
+        return valueOfSupply + getVAT();
+    } 
+ 
+    public double getVAT() {
+        return valueOfSupply * vatRate;
+    }
 }
-
 public class AccountingClassApp {
-
-	public static void main(String[] args) {
-		Accounting.valueOfSupply = 10000.0;
-		Accounting.vatRate = 0.1;
-		Accounting.expenseRate = 0.3;		
-		Accounting.print();
-		// anotherVariable = ...;
-		// anotherMethod = ...;
-	}
-
-	
-
+     
+    public static void main(String[] args) {
+        // instance 
+        Accounting a1 = new Accounting();
+        a1.valueOfSupply = 10000.0;
+        a1.vatRate = 0.1;
+        a1.expenseRate = 0.3;
+        a1.print();
+         
+        Accounting a2 = new Accounting();
+        a2.valueOfSupply = 20000.0;
+        a2.vatRate = 0.05;
+        a2.expenseRate = 0.2;
+        a2.print();
+         
+        a1.print();
+    }
 }
