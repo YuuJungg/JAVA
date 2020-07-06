923da2c51b28ea0262b2c4cd9ef55e5cbb2c36e9
YuuJungg <65105002+YuuJungg@users.noreply.github.com>
2020-07-06 23:04:08 +0900

    Add files via upload

---
+++ 자바학습/PackageEx/src/lib/Calculator.java
@@ -0,0 +1,8 @@
+package lib; // Calculator.java를 컴파일하여 Calculator.class를 lib 폴더에 저장하라는 지시
+
+public abstract class Calculator {
+	public abstract int add(int a, int b);
+	public abstract int subtract(int a, int b);
+	public abstract double average(int[] a);
+
+}
