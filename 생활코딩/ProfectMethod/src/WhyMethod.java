b75efa3d99ac76cc789e9a0070c900104ae86488
YuuJungg <65105002+YuuJungg@users.noreply.github.com>
2020-08-01 12:14:41 +0900

    Add files via upload

---
+++ 생활코딩/ProfectMethod/src/WhyMethod.java
@@ -0,0 +1,17 @@
+
+public class WhyMethod {
+	
+	public static void printTwoTimesA() {
+		System.out.println("-");
+		System.out.println("A");
+		System.out.println("A");
+	}
+	
+	public static void main(String[] args) {		
+		printTwoTimesA();
+		printTwoTimesA();
+		printTwoTimesA();
+		printTwoTimesA();
+	}
+
+}
